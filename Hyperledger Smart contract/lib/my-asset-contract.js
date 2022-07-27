dc9e204b3a1b62a2b8b157352d32050edaab6c47
Ayush Verma <ay322266@dal.ca>
2022-07-26 21:33:03 -0300

    Added code for both systems

---
+++ Hyperledger Smart contract/lib/my-asset-contract.js
@@ -0,0 +1,36 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+"use strict";
+
+const { Contract } = require("fabric-contract-api");
+
+class MyAssetContract extends Contract {
+    async myAssetExists(ctx, myAssetId) {
+        const buffer = await ctx.stub.getState(myAssetId);
+        return !!buffer && buffer.length > 0;
+    }
+
+    async writeSignature(ctx, myAssetId, value) {
+        const exists = await this.myAssetExists(ctx, myAssetId);
+        if (exists) {
+            throw new Error(`The my asset ${myAssetId} already exists`);
+        }
+        const asset = { value };
+        const buffer = Buffer.from(JSON.stringify(asset));
+        await ctx.stub.putState(myAssetId, buffer);
+    }
+
+    async readSignature(ctx, myAssetId) {
+        const exists = await this.myAssetExists(ctx, myAssetId);
+        if (!exists) {
+            throw new Error(`The my asset ${myAssetId} does not exist`);
+        }
+        const buffer = await ctx.stub.getState(myAssetId);
+        const asset = JSON.parse(buffer.toString());
+        return asset;
+    }
+}
+
+module.exports = MyAssetContract;
